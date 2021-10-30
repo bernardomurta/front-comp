e3d77583a1fb1836ce7a38eb2ccc443c6879c0d0
Bernardo Murta <bkatiaia@yahoo.com>
2021-10-30 19:33:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/*  abre e fecha o menu quando clicar no icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Carrossel imgs */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
